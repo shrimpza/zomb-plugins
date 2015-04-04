5321c9029e918fedab4495aba34bafb9b791a936
Kenneth Watson <git@shrimpworks.za.net>
2015-04-04 15:35:27 +0200

    add math plugin

---
+++ php/math.php
@@ -0,0 +1,48 @@
+<?php
+
+	require_once('lib/evalmath.class.php');
+
+	if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+		$plugin	= array(
+			'plugin' =>	'math',
+			'help' => 'evaluate math expressions',
+			'contact' => 'http://shrimpworks.za.net/',
+			'commands' => array(
+				array(
+					'command' => 'calc',
+					'help' => 'evaluate a given math expression and provide the answer',
+					'args' => 0,
+					'pattern' => ''
+				)
+			)
+		);
+		echo json_encode($plugin);
+
+	} else if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+		$request = file_get_contents('php://input');
+		$q = json_decode($request, true);
+		
+		if ($q['command'] == 'calc')	{
+			if (empty($q['args'])) {
+				$res = 'No expression provided, please provide a math expression';
+			} else {
+				$m = new EvalMath();
+				$res = $m->evaluate($q['args'][0]);
+			}
+
+			respond($res);
+		}
+	}
+
+	function respond($response, $image = '', $exit = true) {
+		$res = array(
+			'response' => array($response),
+			'image'	=> $image
+		);
+
+		echo json_encode($res);
+
+		if ($exit) exit;
+	}
+
+?>
